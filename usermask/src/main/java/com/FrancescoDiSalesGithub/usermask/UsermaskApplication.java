f66672c96f07f18528912d1802a781f848c3ffa9
FrancescoDiSalesGithub <f.disales@hotmail.com>
2021-11-21 19:15:44 +0100

    init project

---
+++ usermask/src/main/java/com/FrancescoDiSalesGithub/usermask/UsermaskApplication.java
@@ -0,0 +1,13 @@
+package com.FrancescoDiSalesGithub.usermask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermaskApplication.class, args);
+	}
+
+}
---
+++ usermask/src/test/java/com/FrancescoDiSalesGithub/usermask/UsermaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FrancescoDiSalesGithub.usermask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
